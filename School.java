373fce7e0c774fe8b6dccfd1fc68685863feb978
alaa-hani101 <alaahani797@gmail.com>
2024-06-20 19:02:29 +0300

    refactoring files

--- School.java
+++ School.java
@@ -1,65 +1,65 @@
-
-package school.managment.system;
+package SchoolManagementSystem;
 
 import java.util.List;
 
-public class School {
-   private List<Teacher> teachers;
-   private List<Student> students;
-   private static double TotalMonyEarned;
-   private static double TotalMonySpent;
 
-   public School(List<Teacher> teachers, List<Student> students) {
-        this.teachers = teachers;
-        this.students = students;
-        this.TotalMonyEarned=0;
-        this.TotalMonySpent=0;
-   }
+    public  class School {
+        private List<Teacher> teachers;
+        private List<Student> students;
+        private static double TotalMonyEarned;
+        private static double TotalMonySpent;
 
-    public static  double getTotalMonyEarned() {
-        return TotalMonyEarned;
+        public School(List<Teacher> teachers, List<Student> students) {
+            this.teachers = teachers;
+            this.students = students;
+            this.TotalMonyEarned=0;
+            this.TotalMonySpent=0;
+        }
+
+        public static  double getTotalMonyEarned() {
+            return TotalMonyEarned;
+        }
+
+        public static double getTotalMonySpent() {
+            return TotalMonySpent;
+        }
+
+        public static void updateTotalMonyEarned(double MonyEarned) {
+            School.TotalMonyEarned += MonyEarned;
+        }
+
+        public static void updateTotalMonySpent(double MonySpent) {
+            if(School.TotalMonyEarned>0)
+                School.TotalMonyEarned -= MonySpent;
+            School.TotalMonySpent+=MonySpent;
+        }
+        /*
+           now how much mony school has ?
+        */
+        public double RemainMony()
+        {
+            return School.TotalMonyEarned-School.TotalMonySpent;
+        }
+        /*
+        teachers and students
+        */
+        public List<Teacher> getTeachers() {
+            return teachers;
+        }
+
+        public List<Student> getStudents() {
+            return students;
+        }
+
+        public  void addTeachers(Teacher teacher) {
+            teachers.add(teacher);
+        }
+
+        public  void addStudents(Student student) {
+            students.add(student);
+        }
+
+
+
     }
 
-    public static double getTotalMonySpent() {
-        return TotalMonySpent;
-    }
-    
-    public static void updateTotalMonyEarned(double MonyEarned) {
-        School.TotalMonyEarned += MonyEarned;
-    }
-
-    public static void updateTotalMonySpent(double MonySpent) {
-       if(School.TotalMonyEarned>0) 
-           School.TotalMonyEarned -= MonySpent;
-        School.TotalMonySpent+=MonySpent;
-    }
-    /*
-       now how much mony school has ? 
-    */
-    public double RemainMony()
-    {
-        return School.TotalMonyEarned-School.TotalMonySpent;
-    }
-    /* 
-    teachers and students
-    */
-     public List<Teacher> getTeachers() {
-        return teachers;
-    }
-
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    public /*static*/ void addTeachers(Teacher teacher) {
-       teachers.add(teacher);
-    }
-
-    public  void addStudents(Student student) {
-        students.add(student);
-    }
-    
-  
-  
-   
-}
